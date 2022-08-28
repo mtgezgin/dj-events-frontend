c98713017657dfe1c7ca6e5424d678ec405140d9
mtgezgin <mehmetturgutgezgin@gmail.com>
2022-08-28 18:26:56 +0300

    Revise Layout.

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 import styles from '@/styles/layout.module.css';
 export default function Layout({ title, keywords, description, children }) {
+	const router = useRouter();
+
 	return (
 		<div>
 			<Head>
@@ -11,6 +15,7 @@ export default function Layout({ title, keywords, description, children }) {
 				<meta name='description' content={description} />
 			</Head>
 			<Header />
+			{router.pathname === '/' && <Showcase />}
 			<div className={styles.container}>{children}</div>
 			<Footer />
 		</div>
