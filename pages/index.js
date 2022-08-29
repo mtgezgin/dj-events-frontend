817b65e22755e78ea20b816064689a7cfc2ed9b3
mtgezgin <mehmetturgutgezgin@gmail.com>
2022-08-29 18:55:13 +0300

    Revise homepage and all events page.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
+import Link from 'next/link';
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
 
-export default function Home() {
+export default function Home({ events }) {
 	return (
 		<Layout>
-			<h1>Hello World</h1>
+			<h1>Upcoming Events</h1>
+			{events.length === 0 && <h3>No events to show</h3>}
+			{events.map((evt) => (
+				<EventItem key={evt.id} evt={evt} />
+			))}
+
+			{events.length > 0 && (
+				<Link href='/events'>
+					<a className='btn-secondary'>View All Events</a>
+				</Link>
+			)}
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const data = await res.json();
+
+	return {
+		props: {
+			events: data.slice(0, 3),
+			revalidate: 1,
+		},
+	};
 }
